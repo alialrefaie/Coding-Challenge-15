74313517ca49878c021b9f609dd952231930f4e8
Ali Alrefaie <alrefaiea6@gmail.com>
2024-07-05 23:17:26 -0400

    Add personal information section with styled components

---
+++ my-app/src/PersonalInfo.js
@@ -0,0 +1,13 @@
+// src/PersonalInfo.js
+import React from 'react';
+
+function PersonalInfo() {
+    return (
+        <div>
+            <h1>Ali Alrefaie</h1>
+            <p>Junior at USF pursuing a bachelor's degree in Business Analytics & Information Systems with a concentration in Cybersecurity.</p>
+        </div>
+    );
+}
+
+export default PersonalInfo;
