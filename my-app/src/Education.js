2bb8e05f050e6fa758beb1bb6ea7076f4827bf04
Ali Alrefaie <alrefaiea6@gmail.com>
2024-07-06 21:57:29 -0400

    Display educational background in a dedicated section

---
+++ my-app/src/Education.js
@@ -0,0 +1,13 @@
+// src/Education.js
+import React from 'react';
+
+function Education() {
+    return (
+        <div className="education-section">
+            <h2>Education</h2>
+            <p>BS in Business Analytics & Information Systems, University of South Florida, 2023-2025</p>
+        </div>
+    );
+}
+
+export default Education;
